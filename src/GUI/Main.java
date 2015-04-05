6b89b429cfb5947a420a65fa4565dcc1e0d4c85a
elizabethdye <dyeea@hendrix.edu>
2015-04-05 16:12:35 -0500

    More additions

---
+++ src/GUI/Main.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("GUI.fxml"));
+			VBox root = (VBox) loader.load();
+			
+			Scene scene = new Scene(root,640,500);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace(); 
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
