a72b53b5a43c3575e2c6f496e31366989d80edf5
elizabethdye <dyeea@hendrix.edu>
2015-04-05 20:25:08 -0500

    Added Error Window

--- src/GUI/LoginController.java
+++ src/GUI/LoginController.java
@@ -5,12 +5,18 @@ import java.sql.SQLException;
 
 import Model.Model;
 import Model.UserTypes;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class LoginController {
@@ -36,7 +42,8 @@ public class LoginController {
 	
 	@FXML
 	private void login() throws SQLException, IOException{
-		showNewStage();
+		errorWindow();
+//		showNewStage("GUI.fxml");
 //		Enum user = checkUserType();
 //		if (user == UserTypes.PROFESSOR){
 //			startProfView();
@@ -49,23 +56,26 @@ public class LoginController {
 //		}
 	}
 	
-	private void showNewStage() throws IOException {
-		Parent home_page_parent = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+	private void showNewStage(String FXMLFile) throws IOException {
+		Parent home_page_parent = FXMLLoader.load(getClass().getResource(FXMLFile));
 		Scene home_page_scene = new Scene(home_page_parent);
 		Stage app_stage = (Stage) login.getScene().getWindow();
 		app_stage.setScene(home_page_scene);
 		app_stage.show();
 	}
 	 
-	private void startProfView(){
+	private void startProfView() throws IOException{
+		showNewStage("ProfGUI.fxml");
 		//TODO
 	}
 	
-	private void startStudentView(){
+	private void startStudentView() throws IOException{
+		showNewStage("StudentGUI.fxml");
 		//TODO
 	}
 	
 	private void sendError(){
+		errorWindow();
 		//TODO
 	}
 	
@@ -76,4 +86,39 @@ public class LoginController {
 		type = model.getDatabase().getUserType(ID, password);
 		return type;
 	}
+	
+	@FXML
+	public void errorWindow(){
+		Stage newStage = new Stage();
+		VBox root = new VBox();
+		Label nameField = new Label("Error: Invalid username/password.");
+		HBox selection = new HBox();
+		selection.setSpacing(50);
+		Button okButton = new Button("OK");
+//		Button closeButton = new Button("Cancel");
+		okButton.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+    		public void handle(ActionEvent close){
+    			newStage.close();
+			}
+    		
+    	});
+//		closeButton.setOnAction(new EventHandler<ActionEvent>(){
+//    		@Override
+//    		public void handle(ActionEvent close){
+//    			newStage.close();
+//			}
+//    		
+//    	});
+		selection.getChildren().addAll(okButton);//, closeButton);
+		root.getChildren().addAll(nameField, selection);
+
+		Scene stageScene = new Scene(root);
+		VBox.setVgrow(root, Priority.ALWAYS);
+		newStage.setScene(stageScene);
+		newStage.show();
+		newStage.requestFocus();
+		
+	}
+
 }
